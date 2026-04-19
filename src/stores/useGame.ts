0b0f39b5096d650added9417a58520da28b520aa
ra-li-3js <216971164+ra-li-3js@users.noreply.github.com>
2026-04-19 18:13:47 -0300

    Added user Interface
    Decided to remove player name (as I guess linking a scoreboard is not that necessary);
    Added Interface with Restart (increments the seed so no new game is equal unless refreshing the page);
    Reduced unnecessarily large width and height segments from boxGeometry

--- src/stores/useGame.ts
+++ src/stores/useGame.ts
@@ -4,23 +4,20 @@ import { subscribeWithSelector } from "zustand/middleware";
 interface GameState {
   blocksCount: number;
   blockSeed: number;
-  playerName: string;
   startTime: number;
   endTime: number;
   phase: "ready" | "playing" | "ended";
 
   start: () => void;
   restart: () => void;
-  setPlayerName: (name: string) => void;
   end: () => void;
 }
 
 const useGame = create<GameState>()(
   subscribeWithSelector((set) => {
     return {
-      blocksCount: 25,
+      blocksCount: 20,
       blockSeed: 0,
-      playerName: "",
       startTime: 0,
       endTime: 0,
       phase: "ready",
@@ -36,14 +33,12 @@ const useGame = create<GameState>()(
       restart: () => {
         set((state) => {
           if (state.phase === "playing" || state.phase === "ended")
-            return { phase: "ready", blockSeed: Math.random() };
+            return { phase: "ready", blockSeed: state.blockSeed + 1 };
 
           return {};
         });
       },
-      setPlayerName: (name: string) => {
-        set(() => ({ playerName: name }));
-      },
+
       end: () => {
         set((state) => {
           if (state.phase === "playing")
